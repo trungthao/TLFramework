59a9f8dd489e34502cbd6508a9bcd56ca6df3ea7
trungthao <thao.duongtrung@gmail.com>
2021-02-16 15:00:31 +0700

    xay dung base co ban them/sua/xoa

---
+++ src/backend/TL.Domain/Utilities/ConvertUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TL.Domain.Attributes;
+using TL.Domain.Extensions;
+
+namespace TL.Domain.Utilities
+{
+    public class ConvertUtils
+    {
+        public static Dictionary<string, object> ConvertObjectToDictionaryForStore(object param)
+        {
+            var dicParam = new Dictionary<string, object>();
+            var objType = param.GetType();
+            var properties = objType.GetProperties();
+            if (properties != null)
+            {
+                properties = properties.Where(prop => prop.GetCustomAttribute(typeof(NotColumnAttribute), true) == null).ToArray();
+                foreach (var prop in properties)
+                {
+                    var propName = "$" + prop.Name;
+                    object propValue = prop.GetValue(param);
+                    if (prop.PropertyType == typeof(bool))
+                    {
+                        propValue = Convert.ToInt32(propValue);
+                    }
+                    dicParam.AddOrUpdate(propName, propValue);
+                }
+            }
+
+            return dicParam;
+        }
+    }
+}
